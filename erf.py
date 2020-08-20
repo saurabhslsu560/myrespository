78e7481d7f4bbd7b52d1ac5d989e6f393dbc4b84
saurabhslsu560 <69954832+saurabhslsu560@users.noreply.github.com>
2020-08-20 13:37:10 +0530

    Add files via upload

---
+++ erf.py
@@ -0,0 +1,9 @@
+
+try:
+    k=int(input("press one:"))
+    if(k==1):
+        print("rajkiya engineering college,bijnor")
+    else:
+        print("not pressing valid key")
+except Exception as msg:
+    print("you are pressing a alphabet")
